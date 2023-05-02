bd3f8ec3d366fbc08fa4627b040d8270273d717e
JulienPeloton <peloton@lal.in2p3.fr>
2023-05-03 00:12:24 +1000

    Unified interface for sso parameter fitting

--- fink_utils/sso/spins.py
+++ fink_utils/sso/spins.py
@@ -135,61 +135,20 @@ def color_correction_to_V():
 
     return dic
 
-def add_color_correction(pdf):
-    """ Add a new column with color correction `V - band`.
+def compute_color_correction(filters: np.array) -> np.array:
+    """ Return the color correction `V - band` for each measurement.
 
     band --> band + (V - band)
 
     Parameters
     ----------
-    pdf: pd.DataFrame
-        Pandas DataFrame with Fink ZTF data
+    filters: np.array
+        Array with the filter code for each measurement
 
     Returns
     ----------
     out: pd.DataFrame
-        Input Pandas DataFrame with a new column `color_corr`
-    """
-    filts = np.unique(pdf['i:fid'].values)
-    color_sso = np.ones_like(pdf['i:magpsf'])
-    conversion = color_correction_to_V()
-    for i, filt in enumerate(filts):
-        cond = pdf['i:fid'] == filt
-        color_sso[cond] = conversion[filt]
-
-    pdf['color_corr'] = color_sso
-
-    return pdf
-
-def estimate_sso_params(
-        pdf,
-        func,
-        normalise_to_V=False,
-        bounds=([0, 0, 0, 1e-6, 0, -np.pi / 2], [30, 1, 1, 1, 2 * np.pi, np.pi / 2])):
-    """ Fit for phase curve parameters
-
-    Parameters
-    ----------
-    pdf: pd.DataFrame
-        Contain Fink data + ephemerides
-    func: function
-        Parametric function. Currently supported:
-            - func_hg1g2_with_spin
-            - func_hg1g2
-            - func_hg12
-            - func_hg
-    bounds: tuple of lists
-        Parameters boundaries for `func` ([all_mins], [all_maxs]).
-        Defaults are given for `func_hg1g2_with_spin`: (H, G1, G2, R, lambda0, beta0).
-
-    Returns
-    ----------
-    popt: list
-        Estimated parameters for `func`
-    perr: list
-        Error estimates on popt elements
-    chi2_red: float
-        Reduced chi2
+        Array containing the color correction for each measurement
 
     Example
     ---------
@@ -201,7 +160,6 @@ def estimate_sso_params(
     ...     'https://fink-portal.org/api/v1/sso',
     ...     json={
     ...         'n_or_d': '1465',
-    ...         'withEphem': True,
     ...         'output-format': 'json'
     ...     }
     ... )
@@ -209,84 +167,75 @@ def estimate_sso_params(
     # Extract relevant information
     >>> pdf = pd.read_json(io.BytesIO(r.content))
 
-    # Add color correction in the DataFrame
-    >>> pdf = add_color_correction(pdf)
+    # Compute color correction
+    >>> color_to_V = compute_color_correction(pdf['i:fid'].values)
+    >>> assert len(np.unique(color_to_V)) == len(np.unique(pdf['i:fid'].values)), "Filters and colors have different shape!"
+    True
 
-    >>> bounds = ([0, 0, 0, 1e-1, 0, -np.pi/2], [30, 1, 1, 1, 2*np.pi, np.pi/2])
-    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg1g2_with_spin, bounds=bounds)
-    >>> assert popt is not None, "Fit failed!"
-
-    >>> bounds = ([0, 0, 0], [30, 1, 1])
-    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg1g2, bounds=bounds)
-    >>> assert popt is not None, "Fit failed!"
-
-    >>> bounds = ([0, 0], [30, 1])
-    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg12, bounds=bounds)
-    >>> assert popt is not None, "Fit failed!"
-
-    >>> bounds = ([0, 0], [30, 1])
-    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg, bounds=bounds)
-    >>> assert popt is not None, "Fit failed!"
+    >>> assert 0.0 not in color_to_V, "Some filters have no color correction!"
     """
-    if normalise_to_V:
-        ydata = pdf['i:magpsf_red'] + pdf['color_corr']
-        pdf['i:fid'] = 'V'
-    else:
-        ydata = pdf['i:magpsf_red']
+    filts = np.unique(filters)
+    color_sso = np.zeros_like(filters, dtype=float)
+    conversion = color_correction_to_V()
+    for i, filt in enumerate(filts):
+        cond = filters == filt
+        color_sso[cond] = conversion[filt]
 
-    # Fink returns degrees -- convert in radians
-    alpha = np.deg2rad(pdf['Phase'].values)
-    ra = np.deg2rad(pdf['i:ra'].values)
-    dec = np.deg2rad(pdf['i:dec'].values)
-    pha = np.transpose([[i, j, k] for i, j, k in zip(alpha, ra, dec)])
+    return color_sso
 
-    if func.__name__ == 'func_hg1g2_with_spin':
-        nparams = 6
-        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G1, G2, R, alpha, beta) parameters"
-        x = pha
-    elif func.__name__ == 'func_hg1g2':
-        nparams = 3
-        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G1, G2) parameters"
-        x = alpha
-    elif func.__name__ == 'func_hg12':
-        nparams = 2
-        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G12) parameters"
-        x = alpha
-    elif func.__name__ == 'func_hg':
-        nparams = 2
-        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G) parameters"
-        x = alpha
+def build_eqs(x, filters=[], ph=[], rhs=[], func=None):
+    """ Build the system of equations to solve using the HG, HG12, or HG1G2 model
 
-    if not np.alltrue([i == i for i in ydata.values]):
-        popt = [None] * nparams
-        perr = [None] * nparams
-        chisq_red = None
-        return popt, perr, chisq_red
+    Parameters
+    ----------
+    x: list
+        List of parameters to fit for
+    filters: np.array
+        Array of size N containing the filtername for each measurement
+    ph: np.array
+        Array of size N containing phase angles (rad)
+    rhs: np.array
+        Array of size N containing the actual measurements (magnitude)
+    func: callable
+        Model function to use (e.g. `func_hg1g2`)
 
-    try:
-        # TODO: incorporate jacobian and error estimates
-        popt, pcov = curve_fit(
-            func,
-            x,
-            ydata.values,
-            # sigma=pdf['i:sigmapsf'],
-            bounds=bounds,
-            # jac=Dfunc_hg1g2_with_spin
-        )
+    Returns
+    ----------
+    out: np.array
+        Array of size N containing (model - y)
 
-        perr = np.sqrt(np.diag(pcov))
+    Notes
+    ----------
+    the input `x` contains filter dependent variables,
+    that is (Hs and Gs). For example with two bands g & r with the HG1G2 model:
 
-        r = ydata.values - func(x, *popt)
-        chisq = np.sum((r / pdf['i:sigmapsf'])**2)
-        chisq_red = 1. / (len(ydata.values) - 1 - nparams) * chisq
+    ```
+    x = [
+        h_g, g_1_g, g_2_g,
+        h_r, g_1_r, g_2_r
+    ]
+    ```
 
-    except RuntimeError as e:
-        print(e)
-        popt = [None] * nparams
-        perr = [None] * nparams
-        chisq_red = None
+    """
+    filternames = np.unique(filters)
 
-    return popt, perr, chisq_red
+    params = x
+    nparams = len(params) / len(filternames)
+    assert int(nparams) == nparams, "You need to input all parameters for all bands"
+
+    params_per_band = np.reshape(params, (len(filternames), int(nparams)))
+    eqs = []
+    for index, filtername in enumerate(filternames):
+        mask = filters == filtername
+
+        myfunc = func(
+            ph[mask],
+            *params_per_band[index],
+        ) - rhs[mask]
+
+        eqs = np.concatenate((eqs, myfunc))
+
+    return np.ravel(eqs)
 
 def build_eqs_for_spins(x, filters=[], ph=[], ra=[], dec=[], rhs=[]):
     """ Build the system of equations to solve using the HG1G2 + spin model
@@ -348,7 +297,271 @@ def build_eqs_for_spins(x, filters=[], ph=[], ra=[], dec=[], rhs=[]):
 
     return np.ravel(eqs)
 
-def estimate_hybrid_sso_params(
+def estimate_sso_params(
+        magpsf_red, sigmapsf, phase, filters, ra=None, dec=None,
+        model='SHG1G2',
+        normalise_to_V=False,
+        p0=[15.0, 0.15, 0.15, 0.8, np.pi, 0.0],
+        bounds=([0, 0, 0, 1e-1, 0, -np.pi / 2], [30, 1, 1, 1, 2 * np.pi, np.pi / 2])):
+    """ Fit for phase curve parameters
+
+    Under the hood, it uses a `least_square`. Along with the fitted parameters,
+    we also provide flag to assess the quality of the fit:
+
+    Code for quality `fit`:
+    0: success
+    1: bad_vals
+    2: MiriadeFail
+    3: RunTimError
+    4: LinalgError
+
+    Code for quality `status` (least square convergence):
+    -2: failure
+    -1 : improper input parameters status returned from MINPACK.
+    0 : the maximum number of function evaluations is exceeded.
+    1 : gtol termination condition is satisfied.
+    2 : ftol termination condition is satisfied.
+    3 : xtol termination condition is satisfied.
+    4 : Both ftol and xtol termination conditions are satisfied.
+
+    Typically, one would only trust status = 2 and 4.
+
+
+    Parameters
+    ----------
+    magpsf_red: array
+        Reduced magnitude, that is m_obs - 5 * np.log10('Dobs' * 'Dhelio')
+    sigmapsf: array
+        Error estimates on magpsf_red
+    phase: array
+        Phase angle [rad]
+    filters: array
+        Filter name for each measurement.
+    ra: optional, array
+        Right ascension [rad]. Required for SHG1G2 model.
+    dec: optional, array
+        Declination [rad]. Required for SHG1G2 model.
+    model: str
+        Parametric function. Currently supported:
+            - SHG1G2 (default)
+            - HG1G2
+            - HG12
+            - HG
+    normalise_to_V: optional, bool
+        If True, bring all bands to V. Default is False.
+    p0: list
+        Initial guess for input parameters. If there are several bands,
+        we replicate the bounds for all. Note that in the case of SHG1G2,
+        the spin parameters are fitted globally (and not per band).
+    bounds: tuple of lists
+        Parameters boundaries ([all_mins], [all_maxs]).
+        Lists should be ordered as:
+            - SHG1G2: (H, G1, G2, R, alpha, beta)
+            - HG1G2: (H, G1, G2)
+            - HG12: (H, G12)
+            - HG: (H, G)
+        Note that even if there is several bands `b`, we take the same
+        bounds for all H's and G's.
+
+    Returns
+    ----------
+    outdic: dict
+        Dictionary containing reduced chi2, and estimated parameters and
+        error on each parameters.
+    """
+    if normalise_to_V:
+        color = compute_color_correction(filters)
+        ydata = magpsf_red + color
+        filters = np.array(['V'] * len(filters))
+    else:
+        ydata = magpsf_red
+
+    if model == 'SHG1G2':
+        outdic = fit_spin(
+            ydata,
+            sigmapsf,
+            phase,
+            ra,
+            dec,
+            filters,
+            p0=p0,
+            bounds=bounds
+        )
+    elif model in ['HG', 'HG12', 'HG1G2']:
+        outdic = fit_legacy_models(
+            ydata,
+            sigmapsf,
+            phase,
+            filters,
+            model,
+            p0=p0,
+            bounds=bounds
+        )
+    else:
+        raise AssertionError('model {} is not understood. Please choose among: SHG1G2, HG1G2, HG12, HG'.format(model))
+
+    return outdic
+
+
+def fit_legacy_models(
+        magpsf_red, sigmapsf, phase, filters,
+        model,
+        p0=[15, 0.15, 0.15],
+        bounds=([0, 0, 0], [30, 1, 1])):
+    """ Fit for phase curve parameters
+
+    Parameters
+    ----------
+    magpsf_red: array
+        Reduced magnitude, that is m_obs - 5 * np.log10('Dobs' * 'Dhelio')
+    sigmapsf: array
+        Error estimates on magpsf_red
+    phase: array
+        Phase angle [rad]
+    filters: array
+        Filter name for each measurement
+    model: function
+        Parametric function. Currently supported:
+            - func_hg1g2
+            - func_hg12
+            - func_hg
+    bounds: tuple of lists
+        Parameters boundaries for `func` ([all_mins], [all_maxs]).
+        Defaults are given for `func_hg1g2_with_spin`: (H, G1, G2, R, lambda0, beta0).
+
+    Returns
+    ----------
+    popt: list
+        Estimated parameters for `func`
+    perr: list
+        Error estimates on popt elements
+    chi2_red: float
+        Reduced chi2
+
+    Example
+    ---------
+    >>> import io
+    >>> import requests
+    >>> import pandas as pd
+
+    >>> r = requests.post(
+    ...     'https://fink-portal.org/api/v1/sso',
+    ...     json={
+    ...         'n_or_d': '1465',
+    ...         'withEphem': True,
+    ...         'output-format': 'json'
+    ...     }
+    ... )
+
+    # Extract relevant information
+    >>> pdf = pd.read_json(io.BytesIO(r.content))
+
+    # Add color correction in the DataFrame
+    >>> pdf = add_color_correction(pdf)
+
+    >>> bounds = ([0, 0, 0, 1e-1, 0, -np.pi/2], [30, 1, 1, 1, 2*np.pi, np.pi/2])
+    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg1g2_with_spin, bounds=bounds)
+    >>> assert popt is not None, "Fit failed!"
+
+    >>> bounds = ([0, 0, 0], [30, 1, 1])
+    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg1g2, bounds=bounds)
+    >>> assert popt is not None, "Fit failed!"
+
+    >>> bounds = ([0, 0], [30, 1])
+    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg12, bounds=bounds)
+    >>> assert popt is not None, "Fit failed!"
+
+    >>> bounds = ([0, 0], [30, 1])
+    >>> popt, perr, chisq_red = estimate_sso_params(pdf, func=func_hg, bounds=bounds)
+    >>> assert popt is not None, "Fit failed!"
+    """
+    if model == 'HG1G2':
+        func = func_hg1g2
+        nparams = 3
+        params_ = ['H', 'G1', 'G2']
+        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G1, G2) parameters"
+    elif model == 'HG12':
+        func = func_hg12
+        nparams = 2
+        params_ = ['H', 'G12']
+        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G12) parameters"
+    elif model == 'HG':
+        func = func_hg
+        nparams = 2
+        params_ = ['H', 'G']
+        assert len(bounds[0]) == nparams, "You need to specify bounds on all (H, G) parameters"
+
+    ufilters = np.unique(filters)
+
+    params = []
+    for filt in ufilters:
+        tmp = [i + '_{}'.format(str(filt)) for i in params_]
+        params = np.concatenate((params, tmp))
+
+    initial_guess = []
+    for filt in ufilters:
+        initial_guess = np.concatenate((initial_guess, p0))
+
+    lower_bounds = []
+    upper_bounds = []
+    for filt in ufilters:
+        lower_bounds = np.concatenate((lower_bounds, bounds[0]))
+        upper_bounds = np.concatenate((upper_bounds, bounds[1]))
+
+    if not np.alltrue([i == i for i in magpsf_red]):
+        outdic = {'fit': 1, 'status': -2}
+        return outdic
+
+    try:
+        res_lsq = least_squares(
+            build_eqs,
+            x0=initial_guess,
+            bounds=(lower_bounds, upper_bounds),
+            jac='2-point',
+            loss='linear',
+            args=(filters, phase, magpsf_red, func)
+        )
+
+    except RuntimeError:
+        outdic = {'fit': 3, 'status': -2}
+        return outdic
+
+    popt = res_lsq.x
+
+    # estimate covariance matrix using the jacobian
+    try:
+        cov = linalg.inv(res_lsq.jac.T @ res_lsq.jac)
+        chi2dof = np.sum(res_lsq.fun**2) / (res_lsq.fun.size - res_lsq.x.size)
+        cov *= chi2dof
+
+        # 1sigma uncertainty on fitted parameters
+        perr = np.sqrt(np.diag(cov))
+    except np.linalg.LinAlgError:
+        # raised if jacobian is degenerated
+        outdic = {'fit': 4, 'status': res_lsq.status}
+        return outdic
+
+    rms = np.sqrt(np.mean(res_lsq.fun**2))
+
+    # For the chi2, we use the error estimate from the data directly
+    chisq = np.sum((res_lsq.fun / sigmapsf)**2)
+    chisq_red = chisq / (res_lsq.fun.size - res_lsq.x.size - 1)
+
+    outdic = {
+        'chi2red': chisq_red,
+        'rms': rms,
+        'minphase': np.min(phase),
+        'maxphase': np.max(phase),
+        'status': res_lsq.status,
+        'fit': 0
+    }
+    for i in range(len(params)):
+        outdic[params[i]] = popt[i]
+        outdic['err' + params[i]] = perr[i]
+
+    return outdic
+
+def fit_spin(
         magpsf_red, sigmapsf, phase, ra, dec, filters,
         p0=[15.0, 0.15, 0.15, 0.8, np.pi, 0.0],
         bounds=([0, 0, 0, 1e-1, 0, -np.pi / 2], [30, 1, 1, 1, 2 * np.pi, np.pi / 2])):
