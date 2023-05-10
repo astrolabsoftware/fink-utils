f6f237fe9cb728c392acb25827089e7d517bb953
JulienPeloton <peloton@lal.in2p3.fr>
2023-05-10 16:52:03 +0200

    PEP8

--- fink_utils/sso/spins.py
+++ fink_utils/sso/spins.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
-from scipy.optimize import curve_fit
 from scipy.optimize import least_squares
 from scipy import linalg
 
