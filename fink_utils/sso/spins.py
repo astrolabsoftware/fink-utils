25f4ca9fcca49c1fea10e6dca8b3df029b129425
Julien <peloton@lal.in2p3.fr>
2023-05-25 21:41:42 +0200

    Add the RMS per-band in the SSO payload (#50)
    * Add the RMS per-band
    
    * Bump to 0.13.2

--- fink_utils/sso/spins.py
+++ fink_utils/sso/spins.py
@@ -422,7 +422,7 @@ def estimate_sso_params(
     ...    bounds=([0, 0], [30, 1]),
     ...    model='HG',
     ...    normalise_to_V=False)
-    >>> assert len(hg) == 14, "Found {} parameters: {}".format(len(hg), hg)
+    >>> assert len(hg) == 16, "Found {} parameters: {}".format(len(hg), hg)
 
     >>> hg12 = estimate_sso_params(
     ...    pdf['i:magpsf_red'].values,
@@ -433,7 +433,7 @@ def estimate_sso_params(
     ...    bounds=([0, 0], [30, 1]),
     ...    model='HG12',
     ...    normalise_to_V=False)
-    >>> assert len(hg12) == 14, "Found {} parameters: {}".format(len(hg12), hg12)
+    >>> assert len(hg12) == 16, "Found {} parameters: {}".format(len(hg12), hg12)
 
     >>> hg1g2 = estimate_sso_params(
     ...    pdf['i:magpsf_red'].values,
@@ -444,7 +444,7 @@ def estimate_sso_params(
     ...    bounds=([0, 0, 0], [30, 1, 1]),
     ...    model='HG1G2',
     ...    normalise_to_V=False)
-    >>> assert len(hg1g2) == 18, "Found {} parameters: {}".format(len(hg1g2), hg1g2)
+    >>> assert len(hg1g2) == 20, "Found {} parameters: {}".format(len(hg1g2), hg1g2)
 
     >>> shg1g2 = estimate_sso_params(
     ...    pdf['i:magpsf_red'].values,
@@ -455,7 +455,7 @@ def estimate_sso_params(
     ...    np.deg2rad(pdf['i:dec'].values),
     ...    model='SHG1G2',
     ...    normalise_to_V=False)
-    >>> assert len(shg1g2) == 27, "Found {} parameters: {}".format(len(shg1g2), shg1g2)
+    >>> assert len(shg1g2) == 29, "Found {} parameters: {}".format(len(shg1g2), shg1g2)
 
     # You can also combine data into single V band
     >>> shg1g2 = estimate_sso_params(
@@ -467,7 +467,7 @@ def estimate_sso_params(
     ...    np.deg2rad(pdf['i:dec'].values),
     ...    model='SHG1G2',
     ...    normalise_to_V=True)
-    >>> assert len(shg1g2) == 21, "Found {} parameters: {}".format(len(shg1g2), shg1g2)
+    >>> assert len(shg1g2) == 22, "Found {} parameters: {}".format(len(shg1g2), shg1g2)
 
     # If you enter a wrong model name, raise an error
     >>> wrong = estimate_sso_params(
@@ -617,20 +617,25 @@ def fit_legacy_models(
         outdic = {'fit': 4, 'status': res_lsq.status}
         return outdic
 
-    rms = np.sqrt(np.mean(res_lsq.fun**2))
-
     # For the chi2, we use the error estimate from the data directly
     chisq = np.sum((res_lsq.fun / sigmapsf)**2)
     chisq_red = chisq / (res_lsq.fun.size - res_lsq.x.size - 1)
 
     outdic = {
         'chi2red': chisq_red,
-        'rms': rms,
         'minphase': np.min(phase),
         'maxphase': np.max(phase),
         'status': res_lsq.status,
         'fit': 0
     }
+
+    # Total RMS, and per-band
+    rms = np.sqrt(np.mean(res_lsq.fun**2))
+    outdic['rms'] = rms
+    for filt in ufilters:
+        mask = filters == filt
+        outdic['rms_{}'.format(filt)] = np.sqrt(np.mean(res_lsq.fun[mask]**2))
+
     for i in range(len(params)):
         outdic[params[i]] = popt[i]
         outdic['err' + params[i]] = perr[i]
@@ -741,8 +746,6 @@ def fit_spin(
         outdic = {'fit': 4, 'status': res_lsq.status}
         return outdic
 
-    rms = np.sqrt(np.mean(res_lsq.fun**2))
-
     # For the chi2, we use the error estimate from the data directly
     chisq = np.sum((res_lsq.fun / sigmapsf)**2)
     chisq_red = chisq / (res_lsq.fun.size - res_lsq.x.size - 1)
@@ -750,7 +753,6 @@ def fit_spin(
     geo = spin_angle(ra, dec, popt[params.tolist().index('alpha0')], popt[params.tolist().index('delta0')])
     outdic = {
         'chi2red': chisq_red,
-        'rms': rms,
         'minphase': np.min(phase),
         'maxphase': np.max(phase),
         'minCosLambda': np.min(np.abs(geo)),
@@ -759,6 +761,14 @@ def fit_spin(
         'status': res_lsq.status,
         'fit': 0
     }
+
+    # Total RMS, and per-band
+    rms = np.sqrt(np.mean(res_lsq.fun**2))
+    outdic['rms'] = rms
+    for filt in ufilters:
+        mask = filters == filt
+        outdic['rms_{}'.format(filt)] = np.sqrt(np.mean(res_lsq.fun[mask]**2))
+
     for i in range(len(params)):
         outdic[params[i]] = popt[i]
         outdic['err' + params[i]] = perr[i]
