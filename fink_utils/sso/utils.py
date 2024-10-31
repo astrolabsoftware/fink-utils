84e3deab6b4f097a5e743688c55c02c8f8b52fa4
Benoit Carry <benoit.carry@oca.eu>
2024-10-31 23:06:38 +0100

    new function to estimate a/b & a/c (#118)
    * new function to estimate a/b & a/c
    
    * Apply Ruff
    
    ---------
    
    Co-authored-by: JulienPeloton <peloton@lal.in2p3.fr>

--- fink_utils/sso/utils.py
+++ fink_utils/sso/utils.py
@@ -437,6 +437,36 @@ def compute_light_travel_correction(jd, d_obs):
     return jd_lt
 
 
+def estimate_axes_ratio(residuals, R):
+    """Estimate the axes ratio of a SSO from the residuals of the sHG1G2 model and its oblateness R.
+
+    Parameters
+    ----------
+    residuals: np.array
+        Residuals (observed - model) of the SSO with sHG1G2 model
+    R: float
+        Oblateness parameter of the sHG1G2 model
+
+    Returns
+    -------
+    a_b, a_c: float
+        a/b and a/c axes ratio
+    """
+    # Estimate the amplitude of the lightcurve from residuals
+    # Taken at 2 sigma
+    amplitude = np.std(residuals) * 2.0
+
+    # Estimate the a/b ratio
+    a_b = 10 ** (0.4 * (amplitude * 2))
+
+    # Estimate the a/c ratio (and force c<b)
+    a_c = (a_b + 1) / (2 * R)
+    if a_c < a_b:
+        a_c = a_b
+
+    return a_b, a_c
+
+
 if __name__ == "__main__":
     """Execute the unit test suite"""
 
