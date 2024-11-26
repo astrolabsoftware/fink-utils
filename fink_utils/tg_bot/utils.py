47d0ee4c36b4a14e308f691d1f7b50ecd49079c1
JulienPeloton <peloton@lal.in2p3.fr>
2024-11-26 23:28:11 +0100

    Fix return type

--- fink_utils/tg_bot/utils.py
+++ fink_utils/tg_bot/utils.py
@@ -219,7 +219,7 @@ def get_cutout(cutout=None, ztf_id=None, kind="Difference", origin="alert"):
         )
         if not status_check(r, header=ztf_id):
             return io.BytesIO()
-        data = np.log(np.array(r.json()[0]["b:cutoutScience_stampData"], dtype=float))
+        data = np.log(np.array(r.json()["b:cutoutScience_stampData"], dtype=float))
         plt.axis("off")
         plt.imshow(data, cmap="PuBu_r")
         buf = io.BytesIO()
