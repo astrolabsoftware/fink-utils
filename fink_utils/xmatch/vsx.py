856eadc9a8416a1fa2dd8bf7b48df91a94347a12
Julien <peloton@lal.in2p3.fr>
2026-01-29 09:37:26 +0100

    Fix the number of opposition (#217)

--- fink_utils/xmatch/vsx.py
+++ fink_utils/xmatch/vsx.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 def return_list_of_nonstellar() -> list:
     """Non-stellar variable objects (AGN, quasars, supernovae, etc.)
 
